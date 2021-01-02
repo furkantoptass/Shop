e8086e1f7099c68eca0f47fc39578e615e1bc539
Furkan Toptas <toptas.furkan@gmail.com>
2021-01-02 13:31:01 +0300

    Account Service
    We will use to for Login

---
+++ src/app/services/account.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { User } from '../login/user';
+
+@Injectable()
+export class AccountService {
+
+  constructor() { }
+
+  loggedIn= false;
+
+
+  login(user:User):boolean{
+
+    if(user.userName=="furkan" && user.password=="123")
+    {
+      return true;
+      this.loggedIn=true;
+      localStorage.setItem("isLogged",user.userName);
+    }
+    return false;
+
+  }
+
+  isLoggedIn(){
+    return this.loggedIn;
+  }
+
+  logOut(){
+    localStorage.removeItem("isLogged");
+    this.loggedIn=false;
+  }
+
+
+
+
+}
