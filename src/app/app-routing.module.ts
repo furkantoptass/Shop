3abb724cf56fdc0a218f5a65c729110ef7c57d57
Furkan Toptas <toptas.furkan@gmail.com>
2021-01-02 13:34:03 +0300

    Firstly LoginComponent Added
    Firstly login component then we will show AddProductClassicComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { ProductComponent } from './product/product.component';
 import { AddProductFormClassicComponent } from './product/add-product-form-classic/add-product-form-classic.component';
 import { CategoryComponent } from './category/category.component';
 import { AddProductFormReactiveComponent } from './product/add-product-form-reactive/add-product-form-reactive.component';
+import { LoginComponent } from './login/login.component';
+import { LoginGuard } from './login/login.guard';
 
 const routes: Routes = [
   
@@ -11,8 +13,9 @@ const routes: Routes = [
   
   {path:'', redirectTo : 'products', pathMatch : 'full'},
   {path:'products/category/:categoryId', component : ProductComponent},
-  {path:'products/add-product', component : AddProductFormClassicComponent},
-  {path:'products/add-product-reactive', component : AddProductFormReactiveComponent}
+  {path:'products/add-product', component : AddProductFormClassicComponent, canActivate:[LoginGuard]},
+  {path:'products/add-product-reactive', component : AddProductFormReactiveComponent},
+  {path:'login', component : LoginComponent},
 
 ];
 
