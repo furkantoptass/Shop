ce631761d6b719f925afcbd33ed8a0c268f6f361
Furkan Toptas <toptas.furkan@gmail.com>
2021-01-04 15:41:43 +0300

    there are errors
    there are errors so ı will look there.

--- src/app/product/add-product-form-reactive/add-product-form-reactive.component.ts
+++ src/app/product/add-product-form-reactive/add-product-form-reactive.component.ts
@@ -1,84 +1,88 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule,FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, FormControl } from '@angular/forms';
 import { Product } from '../product';
 import { CategoryService } from 'src/app/category/category.service';
 import { Category } from 'src/app/category/category';
 import { ProductService } from 'src/app/services/product.service';
 import { AlertifyService } from 'src/app/services/alertify.service';
 
+
 @Component({
   selector: 'app-add-product-form-reactive',
   templateUrl: './add-product-form-reactive.component.html',
   styleUrls: ['./add-product-form-reactive.component.css'],
-  providers : [CategoryService, ProductService],
+  providers: [CategoryService, ProductService],
 })
 export class AddProductFormReactiveComponent implements OnInit {
-
-  constructor(private formBuilder:FormBuilder, 
-    private categoryService : CategoryService, 
-    private productService : ProductService,
-    private alertifySevice : AlertifyService,
-    public productAddForm : FormGroup
-      
-    )  { 
-
-  
-    }
-
-   
-    // productAddForm = new FormGroup({
-    //   name:new FormControl(),
-    //   desciription:new FormControl(),
-    //   imageUrl:new FormControl(),
-    //   price:new FormControl(),
-    //   categoryId:new FormControl()
-    // });
-
-  productAddForm!:FormGroup;//Look at me
-
-
-    product:Product=new Product();
-    categories: Category[]=[];
-
   
 
-  createproductAddForm(){
-    
-    this.productAddForm=this.formBuilder.group({
-      name:["",Validators.required],
-      desciription:["",Validators.required],
-      imageUrl:["",Validators.required],
-      price:["",Validators.required],
-      categoryId:["",Validators.required],
-      
+  constructor(private formBuilder: FormBuilder,
+    private categoryService: CategoryService,
+    private productService: ProductService,
+    private alertifySevice: AlertifyService,
+    //public productAddForm : FormGroup
+
+  ) {}
+
+  // productAddForm = new FormGroup({
+  //   name: new FormControl() ,
+  //   desciription: new FormControl(),
+  //   imageUrl: new FormControl(),
+  //   price: new FormControl(),
+  //   categoryId: new FormControl()
+  // });
+
+   productAddForm: FormGroup; //Look at me
+  //Look at me
+
+
+  product: Product = new Product();
+  categories: Category[] = [];
+
+
+
+  createproductAddForm() {
+
+    this.productAddForm = this.formBuilder.group({
+      name: [null, Validators.required],
+      desciription: [null, Validators.required],
+      imageUrl: [null, Validators.required],
+      price: [null, Validators.required],
+      categoryId: [null, Validators.required],
+
     });
+
+  
   }
 
   ngOnInit(): void {
 
+  
     this.createproductAddForm();
 
-    this.categoryService.getCategory().subscribe(data=>{
-      this.categories=data;
-      })
+    
+    this.categoryService.getCategory().subscribe(data => {
+      this.categories = data;
+    })
   }
 
 
 
-  add(){
+
+
+  add() {
     console.log("Olta geldi")
-    if(this.productAddForm?.valid)
-    {
-      this.product=Object.assign({},this.productAddForm.value)
-     
+    if (this.productAddForm?.valid) {
+      this.product = Object.assign({}, this.productAddForm.value)
+
     }
 
-    this.productService.addProduct(this.product).subscribe(data=>{
-    this.alertifySevice.success("New Product: "+this.product.name +" Added" );
-  });
+    this.productService.addProduct(this.product).subscribe(data => {
+      this.alertifySevice.success("New Product: " + this.product.name + " Added");
+    });
   }
 
-  
+
 }
 
 
